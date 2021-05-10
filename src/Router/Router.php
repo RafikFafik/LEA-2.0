c6b600b92840b0d4716540239a5f2f4b595cfbbd
Rafał Jaskulski <rafalj@lea24.pl>
2021-05-10 09:16:27 +0200

    routes

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -6,8 +6,8 @@ class Router {
 
     private $routes = [];
 
-    function __construct(){
-        $this->routes = yaml_parse_file(__DIR__.'/../src/config/routes.yaml');
+    function __construct() {
+        $this->routes = file_get_contents('/var/www/LEA2.0/config/routes.json');
 
         die(json_encode($this->routes));
     }
