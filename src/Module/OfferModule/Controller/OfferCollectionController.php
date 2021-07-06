10a214cdb3baf5a12a4affcdc20a768bd6899f34
Rafał Jaskulski <rafalj@lea24.pl>
2021-07-06 09:39:14 +0200

    OfferControllers - cleanings

--- src/Module/OfferModule/Controller/OfferCollectionController.php
+++ src/Module/OfferModule/Controller/OfferCollectionController.php
@@ -3,32 +3,8 @@
 namespace Lea\Module\OfferModule\Controller;
 
 use Lea\Core\Controller\Controller;
-use Lea\Request\Request;
-use Lea\Response\Response;
-use Lea\Core\Serializer\Normalizer;
-use Lea\Module\OfferModule\Entity\Offer;
 use Lea\Core\Controller\ControllerInterface;
-use Lea\Module\OfferModule\Repository\OfferRepository;
 
 class OfferCollectionController extends Controller implements ControllerInterface
 {
-    public function init(): void
-    {
-        switch ($this->request->method()) {
-            case "GET":
-                $offer_repository = new OfferRepository();
-                $list = $offer_repository->findList();
-                $res = Normalizer::denormalizeList($list);
-                Response::ok($res);
-            case "POST":
-                $object = Normalizer::normalize($this->request->getPayload(), Offer::getNamespace());
-                $repository = new OfferRepository();
-                $id = $repository->save($object);
-                $res = $repository->findById($id, new Offer);
-                $res = Normalizer::denormalize($res);
-                Response::ok($res);
-            default:
-                Response::methodNotAllowed();
-        }
-    }
 }
--- src/Module/OfferModule/Controller/OfferController.php
+++ src/Module/OfferModule/Controller/OfferController.php
@@ -3,53 +3,7 @@
 namespace Lea\Module\OfferModule\Controller;
 
 use Lea\Core\Controller\Controller;
-use Lea\Request\Request;
-use Lea\Response\Response;
-use Lea\Module\OfferModule\Entity\Offer;
 use Lea\Core\Controller\ControllerInterface;
-use Lea\Core\Exception\ResourceNotExistsException;
-use Lea\Core\Serializer\Normalizer;
-use Lea\Module\OfferModule\Repository\OfferRepository;
-
 class OfferController extends Controller implements ControllerInterface
 {
-    public function init(): void
-    {
-        switch ($this->request->method()) {
-            case "GET":
-                try {
-                    $offerRepository = new OfferRepository();
-                    $object = $offerRepository->findById($this->params['id'], new Offer);
-                    $result = Normalizer::denormalize($object);
-                    Response::ok($result);
-                } catch (ResourceNotExistsException $e) {
-                    Response::badRequest();
-                } finally {
-                    Response::badRequest("Coś nie tak");
-                }
-                break;
-            case "POST":
-            case "PUT":
-                try {
-                    $repository = new OfferRepository();
-                    $object = Normalizer::normalize($this->request->getPayload(), Offer::getNamespace());
-                    $affected_rows = $repository->updateById($object, $this->params['id']);
-                    Response::noContent();
-                } catch (ResourceNotExistsException $e) {
-                    Response::badRequest();
-                }
-                break;
-            case "DELETE":
-                try {
-                    $offerRepository = new OfferRepository();
-                    $offerRepository->removeById($this->params['id']);
-                    Response::noContent();
-                } catch (ResourceNotExistsException $e) {
-                    Response::badRequest();
-                }
-                break;
-            default:
-                Response::methodNotAllowed();
-        }
-    }
 }
