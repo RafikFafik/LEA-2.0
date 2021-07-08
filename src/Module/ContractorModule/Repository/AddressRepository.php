fba3066afc98cf0d384ac004f706130932cf6b85
Rafał Jaskulski <rafalj@lea24.pl>
2021-07-08 13:43:03 +0200

    flat contractor list

---
+++ src/Module/ContractorModule/Repository/AddressRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lea\Module\ContractorModule\Repository;
+
+use Lea\Core\Repository\Repository;
+use Lea\Module\ContractorModule\Entity\Address;
+
+final class AddressRepository extends Repository
+{
+    public function findByContractorId(int $contractor_id): Address
+    {
+        $result = $this->getRecordData($contractor_id, 'contractor_id');
+
+        return $result;
+    }
+}
--- src/Module/ContractorModule/Repository/ContractorRepository.php
+++ src/Module/ContractorModule/Repository/ContractorRepository.php
@@ -5,6 +5,25 @@ declare(strict_types=1);
 namespace Lea\Module\ContractorModule\Repository;
 
 use Lea\Core\Repository\Repository;
+use Lea\Core\Security\Repository\UserRepository;
+
 final class ContractorRepository extends Repository
 {
+    public function findContractorList(): iterable
+    {
+        $address_repository = new AddressRepository;
+        $user_repository = new UserRepository;
+        $list = $this->findFlatList();
+        foreach($list as $obj) {
+            $address = $address_repository->findByContractorId($obj->getId());
+            $user = $user_repository->findById($obj->getAdvisor());
+            $obj->address = $address->getAddress();
+            $obj->voivodeship = $address->getVoivodeship();
+            $obj->voivodeship = $address->getVoivodeship();
+            $obj->city = $address->getCity();
+            $obj->guardian = $user->getName() . " " . $user->getSurname();
+        }
+
+        return $list;
+    }
 }
