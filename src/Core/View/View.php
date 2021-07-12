8d4e75aa5b46de7d4ab97aa7a6f6e670688bec8c
Rafał Jaskulski <rafalj@lea24.pl>
2021-07-12 16:01:54 +0200

    View WIP

---
+++ src/Core/View/View.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lea\Core\View;
+
+
+abstract class View implements ViewInterface
+{
+}
---
+++ src/Module/ContractorModule/View/ContractorView.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lea\Module\ContractorModule\View;
+
+use Lea\Core\View\View;
+
+class ContractorView extends View
+{
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $shortname;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $fullname;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $nip;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $email;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $advisor;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $register_number;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $address;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $voivodeship;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $city;
+    
+    /**
+     * @from Contractor
+     * @property $shortname
+     */
+    private $guardian;
+}
