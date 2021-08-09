bbeaeb9e8152d606a40ff08692c5ed1c51b9572f
Rafał Jaskulski <rafalj@lea24.pl>
2021-08-09 13:16:12 +0200

    project view - new fields

--- src/Module/ProjectModule/View/Project.php
+++ src/Module/ProjectModule/View/Project.php
@@ -10,24 +10,46 @@ use Lea\Core\View\View;
 class Project extends View
 {
   /**
+   * @from Project
    * @var string
    */
   private $name;
 
   /**
+   * @from Project
    * @var int
    */
   private $cost;
 
   /**
+   * @from Project
    * @var string
    */
   private $status;
 
   /**
+   * @from Project
    * @var string
    */
   private $description;
+
+  /**
+   * @from Contractor
+   * @var string
+   */
+  private $contractor_shortname;
+
+  /**
+   * @from ContractorEmployee ( name + surname )
+   * @var string
+   */
+  private $guardian;
+
+  /**
+   * @from Project
+   * @var Date
+   */
+  private $expiration_date;
 
   /**
    * Get the value of name
@@ -121,6 +143,74 @@ class Project extends View
   public function setDescription(string $description)
   {
     $this->description = $description;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of contractor_shortname
+   *
+   * @return  string
+   */ 
+  public function getContractorShortname()
+  {
+    return $this->contractor_shortname;
+  }
+
+  /**
+   * Set the value of contractor_shortname
+   *
+   * @param  string  $contractor_shortname
+   *
+   * @return  self
+   */ 
+  public function setContractorShortname(string $contractor_shortname)
+  {
+    $this->contractor_shortname = $contractor_shortname;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of guardian
+   */ 
+  public function getGuardian()
+  {
+    return $this->guardian;
+  }
+
+  /**
+   * Set the value of guardian
+   *
+   * @return  self
+   */ 
+  public function setGuardian(string $guardian)
+  {
+    $this->guardian = $guardian;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of expiration_date
+   *
+   * @return  Date
+   */ 
+  public function getExpirationDate()
+  {
+    return $this->expiration_date;
+  }
+
+  /**
+   * Set the value of expiration_date
+   *
+   * @param  Date  $expiration_date
+   *
+   * @return  self
+   */ 
+  public function setExpirationDate(Date $expiration_date)
+  {
+    $this->expiration_date = $expiration_date;
 
     return $this;
   }
